76740f9ea81eada44d75a5205cc0d900fc10b522
vanderms <vanderms.84@gmail.com>
2022-01-27 20:22:08 -0300

    add meta tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -33,6 +33,31 @@ const Home: NextPage<Props> = ({ activities }) => {
         <meta name='description' content='Frontend Mentor | Time tracking dashboard' />
         <link rel='icon' type='image/png' sizes='32x32' href='./images/favicon-32x32.png' />
         <title>Frontend Mentor | Time tracking dashboard</title>
+
+        {/* <!-- Site Name, Title, and Description to be displayed --> */}
+        <meta property='og:site_name' content='Frontend Mentor | Time tracking dashboard ' />
+        <meta property='og:title' content='Time tracking dashboard' />
+        <meta
+          property='og:description'
+          content='My solution to the challenge Time Tracking dashboard, by Frontend Mentor'
+        />
+
+        {/* <!-- Image to display -->  */}
+        {/* <!-- Replace   «example.com/image01.jpg» with your own -->  */}
+        <meta
+          property='og:image'
+          content='https://vanderms.github.io/frontend-mentor-io-time-tracking-dashboard/design/desktop-design.jpg'
+        />
+
+        {/* <!-- No need to change anything here -->  */}
+        <meta property='og:type' content='website' />
+        <meta property='og:image:type' content='image/jpeg' />
+
+        {/* <!-- Website to visit when clicked in fb or WhatsApp-->  */}
+        <meta
+          property='og:url'
+          content='https://vanderms.github.io/frontend-mentor-io-time-tracking-dashboard/'
+        ></meta>
       </Head>
       <main>
         <Sidebar timeframe={timeframe} setTimeframe={setTimeframe} />
