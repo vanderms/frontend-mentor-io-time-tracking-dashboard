97578b2ec61c6cf92554ad4c0d68af7eeda5587e
vanderms <vanderms.84@gmail.com>
2022-01-27 19:49:42 -0300

    remove out directory from gitignore

---
+++ _next/static/8gfuVRLSFptvs1_n7kggj/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-9ae95f028f9e4f44.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
