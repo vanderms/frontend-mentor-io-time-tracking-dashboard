41fc4fe9ac11a7f7f796223709d01c8b64569f33
vanderms <vanderms.84@gmail.com>
2022-01-27 20:22:08 -0300

    add meta tags

--- _next/static/8gfuVRLSFptvs1_n7kggj/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-9ae95f028f9e4f44.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
